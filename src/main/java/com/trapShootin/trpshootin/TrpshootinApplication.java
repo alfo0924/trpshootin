8bc4638293ae43d977e028266633a3b238c21ed4
alfo0924 <halo0924@yahoo.com.tw>
2024-12-13 15:13:54 +0800

    v1.01 initial commit and push

---
+++ src/main/java/com/trapShootin/trpshootin/TrpshootinApplication.java
@@ -0,0 +1,13 @@
+package com.trapShootin.trpshootin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrpshootinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrpshootinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trapShootin/trpshootin/TrpshootinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trapShootin.trpshootin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrpshootinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
