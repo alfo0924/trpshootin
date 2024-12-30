f03c5d314c253b8ca8f420c2eecbd862b46f6b86
alfo0924 <halo0924@yahoo.com.tw>
2024-12-30 09:54:27 +0800

    v1.101

--- src/main/java/com/trapShootin/trpshootin/TrpshootinApplication.java
+++ src/main/java/com/trapShootin/trpshootin/TrpshootinApplication.java
@@ -3,11 +3,8 @@ package com.trapShootin.trpshootin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class TrpshootinApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(TrpshootinApplication.class, args);
+		TrapShooting.main(args);
 	}
-
 }
